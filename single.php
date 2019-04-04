a6734449ef3ae7b7aa0b61e7933cb6057d2091c5
Jimmy Soller <james.soller@gmail.com>
2019-04-04 12:07:16 -0400

    Create single.php

---
+++ single.php
@@ -0,0 +1,17 @@
+   <div class="post-body">
+                            <!-- [SINGLEPOST POST CONTENT] [START]-->
+                            <div class="post-content">
+                                
+                             <?php the_post_thumbnail(); ?>
+                                
+                             <?php the_content(); ?>
+                                
+
+                               <?php
+
+//                               echo $post->post_content
+                               the_field("description");
+
+
+
+ </div>
